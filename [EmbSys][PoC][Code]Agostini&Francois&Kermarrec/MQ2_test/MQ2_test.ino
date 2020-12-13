ff419710bfdbbbd4be447632a4a46f2114c37a66
francoiskermarrec <75950614+francoiskermarrec@users.noreply.github.com>
2020-12-13 20:54:00 +0100

    Add files via upload

---
+++ [EmbSys][PoC][Code]Agostini&Francois&Kermarrec/MQ2_test/MQ2_test.ino
@@ -0,0 +1,11 @@
+const int mq2Pin = 34;
+
+void setup()
+{
+    Serial.begin(115200); 
+}
+
+void loop()
+{
+    Serial.println(analogRead(mq2Pin));
+    delay(1000);        
---
+++ [EmbSys][PoC][Code]Agostini&Francois&Kermarrec/Sound_Sensor/Sound_Sensor.ino
@@ -0,0 +1,11 @@
+
+void setup(){
+  Serial.begin (115200);
+}
+  
+void loop (){
+  int val;
+  val=analogRead(13);  
+  Serial.println(val);
+  delay(100);
+}
